0e473c2749e415b99be6251e9c9640e2ca983f92
ibu1224 <ibuki.tdu1224@gmail.com>
2017-05-28 16:02:54 +0900

    create

--- tdu/tdu/urls.py
+++ tdu/tdu/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('home.urls')),
+    #url(r'^profile/', include('profiles.urls')),
+    url(r'^regist/', include('accounts.urls', namespace="accounts")),
+    url(r'^app', include('app.urls', namespace='app')),
 ]
