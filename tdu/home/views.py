538cf43e1c845fc42861e61297742474b7b2af24
松本寛輝 <matsumotohiroki@wauth264.14-44-22.wlan.ms.dendai.ac.jp>
2017-06-10 12:55:19 +0900

    homeに最新のスレッド5件までを表示 homeに掲示板へのurlリンクを作成 掲示板の投稿者名に初期値"@名無しの電大生"を追加

--- tdu/home/views.py
+++ tdu/home/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from keijiban.models import Posting
+from polls.models import Poll
+
 # Create your views here.
 
 def show(request):
-    return render(request,'home/home.html')
+    posts_list = Posting.objects.order_by('-created_at')[:5]
+    db_poll = Poll.objects.all()
+    return render(request,'home/home.html',{'posts_list': posts_list,'db_poll': db_poll})
