cf904d30f3f7283e132b16a35b6835b1d206939a
ibu1224 <ibuki.tdu1224@gmail.com>
2017-07-04 17:27:52 +0900

    encrypt

--- tdu/profiles/views.py
+++ tdu/profiles/views.py
@@ -5,7 +5,7 @@ from .forms import UserProfileForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
+from django.http import HttpResponse
 def profile_detail(request, pk):
     post = get_object_or_404(UserProfile, pk=pk)
     #return HttpResponse(request.user.email)
@@ -21,13 +21,19 @@ def profile_mydetail(request):
 def profile_edit(request):
     email = request.user.email
     post = UserProfile.objects.get(email = email)
-
     if request.method == "POST":
-        form = UserProfileForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            return redirect('profile_mydetail')
+
+        #form = UserProfileForm(request.POST, instance=post)
+        post.name = request.POST["name"]
+        post.text = request.POST["text"]
+        post.major = request.POST["major"]
+        post.grade = request.POST["grade"]
+        post.save()
+        # if form.is_valid():
+        #     post = form.save(commit=False)
+        #     post.save()
+        #     return redirect('profile_mydetail')
+        return redirect('profile_mydetail')
     else:
         form = UserProfileForm(instance=post)
     return render(request, 'profiles/profile_edit.html', {'form': form})
